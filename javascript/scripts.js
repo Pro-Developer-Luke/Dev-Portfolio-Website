17bc223ff31219eb7594b1d4a0501781dc4f2a97
Luke Wood <addymy@hotmail.co.uk>
2024-08-05 16:57:46 +0100

    Added To-Top Feature

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
         item.style.animationDelay = `${index * 200}ms`; // Stagger the animation
     });
 });
+
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+	if(window.pageYOffset > 500) {
+		toTop.classList.add("active");
+	} else{
+		toTop.classList.remove("active");
+	}
+})
